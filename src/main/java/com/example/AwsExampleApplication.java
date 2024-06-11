600a153721b8f3436de90097c2d5da526fb086ec
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-06-11 20:29:09 +0100

    Add hello API

---
+++ src/main/java/com/example/AwsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsExampleApplication.class, args);
+	}
+
+}
